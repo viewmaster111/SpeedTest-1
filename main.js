0b475870d0004987db35493fc08b451573ce7930
jdc20181 <jdc20181@gmail.com>
2016-06-01 12:56:45 -0400

    Update main.js
    Added a different picture.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-<!--Image is public domain-->
-var imageAddr = "imagetester.jpg"; 
+<!--Image is a ScreenShot-->
+var imageAddr = "http://screenshot.sh/mMztUAXu7m6cm"; 
 var downloadSize = 4995374; //bytes
 function ShowProgressMessage(msg) {
     if (console) {
