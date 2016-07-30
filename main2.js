af68c8aa401cd7680a7d25f90dfa3ff2b6e5a34a
jdc20181 <jdc20181@gmail.com>
2016-07-30 17:53:30 -0400

    Update main2.js

--- main2.js
+++ main2.js
@@ -47,8 +47,8 @@ function MeasureConnectionSpeed() {
         var duration = (endTime - startTime) / 1000;
         var bitsLoaded = downloadSize * 8;
         var speedBps = (bitsLoaded / duration).toFixed(2);
-        var speedKbps = (speedBps / 311000).toFixed(2);
-        var speedMbps = (speedKbps / 311000).toFixed(2);
+        var speedKbps = (speedBps / 1024).toFixed(2);
+        var speedMbps = (speedKbps / 1024).toFixed(2);
         ShowProgressMessage([
             "Your connection speed is:", 
             speedBps + " bps", 
