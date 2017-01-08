fe2e972b50cc60c0df3c34345cfb96c1edc6bc63
jdc20181 <jdc20181@gmail.com>
2017-01-07 20:40:52 -0500

    Create assets.js

---
+++ Beta/assets.js
@@ -0,0 +1,2 @@
+//NOT OFFICIAL PART OF SPEEDTEST This is Beta Assets. Anything Js, and is part of the beta -
+//will be here. Everything essential to proper working will remain in the Beta.js file. 
