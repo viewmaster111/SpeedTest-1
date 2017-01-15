85a6bb44cde3fef1e94d5b0c3e65c673784b8c8e
jdc20181 <jdc20181@gmail.com>
2017-01-15 11:26:29 -0500

    Create progress.js

---
+++ progress.js
@@ -0,0 +1,16 @@
+function move() {
+  var elem = document.getElementById("myBar");   
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+      window.location = "http://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_progressbar_label_js";
+    } else {
+      width++; 
+      elem.style.width = width + '%'; 
+      document.getElementById("label").innerHTML = width * 1  + '%';
+    }
+    
+  }
+}
