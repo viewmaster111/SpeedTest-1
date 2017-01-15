584a4b1005e22bfd5b497b34060d1ea876968443
jdc20181 <jdc20181@gmail.com>
2017-01-15 18:13:40 -0500

    Update progress.js

--- progress.js
+++ progress.js
@@ -1,7 +1,7 @@
 function move() {
   var elem = document.getElementById("myBar");   
   var width = 1;
-  var id = setInterval(frame, 3);
+  var id = setInterval(frame, 100);
   function frame() {
     if (width >= 100) {
       clearInterval(id);
