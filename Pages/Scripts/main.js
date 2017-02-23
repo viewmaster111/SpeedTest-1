1ff4ec93ad373904876744f8244bb6cd3e043edd
jdc20181 <jdc20181@gmail.com>
2017-02-22 22:38:44 -0500

    Experiment #3 Final Phase Part 1
    Increased varible again - Hoping to inprove results.

--- Pages/Scripts/main.js
+++ Pages/Scripts/main.js
@@ -44,7 +44,7 @@ function MeasureConnectionSpeed() {
     
  function showResults() {
         var duration = (endTime - startTime) / 1000;
-        var bitsLoaded = downloadSize * 10 ; /* Experment: Change Var from 8 to 10 */
+        var bitsLoaded = downloadSize * 15 ; /* Experment: Change Var from 8 to 15 */
         var speedBps = (bitsLoaded / duration);
         var speedKbps = (speedBps / 1024);
         var speedMbps = (speedKbps / 1024);
