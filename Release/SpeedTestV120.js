3653c9d17fce879ebcc4dec5acd7019fcb1ace40
jdc20181 <jdc20181@gmail.com>
2017-02-25 14:45:01 -0500

    Create SpeedTestV120.js

---
+++ Release/SpeedTestV120.js
@@ -0,0 +1,56 @@
+var imageAddr = "https://jdc20181.github.io/testpics/winter-fog-1323462087o5i.jpg"; 
+var downloadSize = 584000; //bytes
+function ShowProgressMessage(msg) {
+    if (console) {
+        if (typeof msg == "string") {
+            console.log(msg);
+        } else {
+            for (var i = 0; i < msg.length; i++) {
+                console.log(msg[i]);
+            }
+        }
+    }
+    
+    var oProgress = document.getElementById("progress");
+    if (oProgress) {
+        var actualHTML = (typeof msg == "string") ? msg : msg.join("<br />");
+        oProgress.innerHTML = actualHTML;
+    }
+}
+function InitiateSpeedDetection() {
+    ShowProgressMessage("Loading the image, please wait...");
+    window.setTimeout(MeasureConnectionSpeed, 1);
+};    
+if (window.addEventListener) {
+    window.addEventListener('load', InitiateSpeedDetection, false);
+} else if (window.attachEvent) {
+    window.attachEvent('onload', InitiateSpeedDetection);
+}
+function MeasureConnectionSpeed() {
+    var startTime, endTime;
+    var download = new Image();
+    download.onload = function () {
+        endTime = (new Date()).getTime();
+        showResults();
+    }
+    
+    download.onerror = function (err, msg) {
+        ShowProgressMessage("Sorry there is something wrong, image error or download error. ");
+    }
+    
+    startTime = (new Date()).getTime();
+    var cacheBuster = "?nnn=" + startTime;
+    download.src = imageAddr + cacheBuster;
+    
+ function showResults() {
+        var duration = (endTime - startTime) / 1000;
+        var bitsLoaded = downloadSize * 13; /*Adjustment Varible 8 to 13*/
+        var speedBps = (bitsLoaded / duration);
+        var speedKbps = (speedBps / 1024);
+        var speedMbps = (speedKbps / 1024);
+        ShowProgressMessage([
+            "Your connection speed is:", 
+            speedMbps.toFixed(2) + " Mbps",
+        ]);
+}
+}
