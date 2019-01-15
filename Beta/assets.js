8f2db2fe03937bdd75bb72534428e5ee30f62e6a
jdc20181 <jdc20181@gmail.com>
2019-01-15 11:54:46 -0500

    Delete assets.js

--- Beta/assets.js
+++
@@ -1,2 +0,0 @@
-//NOT OFFICIAL PART OF SPEEDTEST This is Beta Assets. Anything Js, and is part of the beta -
-//will be here. Everything essential to proper working will remain in the Beta.js file. 
